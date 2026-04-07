e3d5e1a35ad33a8daf9faadf0217891f5192050e
Jose Moreno <joseangelmoreno1333@gmail.com>
2026-04-07 18:24:00 -0400

    xd

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://inventarioenwebxd.netlify.app", "http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/ubicaciones", ubicacionRoutes);
